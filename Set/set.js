bcb3aed4c12a27b88f87b38f9f12c70d70c36fb2
Jitesh23 <jiteshpowankar@gmail.com>
2018-07-17 01:37:51 +0530

    Created Set Class and testetd the methods

--- Set/set.js
+++ Set/set.js
@@ -13,7 +13,7 @@ function Set() {
         }
         return false;
     };
-    
+
     this.remove = function (value) {
 
         if (this.has(value)){
@@ -44,6 +44,77 @@ function Set() {
         }
         return values;
     }
+
+    this.union = function (otherset) {
+
+      let unionSet = new Set();
+
+      let values = this.values();
+
+        for(let i = 0; i< values.length; i++){
+          unionSet.add(values[i]);
+
+        }
+
+        values = otherset.values();
+
+        for (var i = 0; i < values.length; i++) {
+          unionSet.add(values[i]);
+        }
+
+      return unionSet;
+    }
+
+    this.intersection = function (otherSet) {
+
+      let interSection = new Set();
+
+      let values = this.values();
+
+      for (var i = 0; i < values.length; i++) {
+        if (otherSet.has(values[i]))
+        interSection.add(values[i]);
+      }
+      return interSection;
+}
+
+this.difference = function (setB) {
+
+  let differenceSet = new Set();
+
+  let values = this.values();
+
+
+  for (var i = 0; i < values.length; i++) {
+
+    if (!setB.has(values[i])) {
+      differenceSet.add(values[i]);
+    }
+  }
+  return differenceSet;
+}
+
+this.subset = function (otherSet) {
+
+let values = this.values();
+
+  if (this.size() > otherSet.size()){
+    return false;
+  }else {
+
+  for (var i = 0; i < values.length; i++) {
+
+    if(!otherSet.has(values[i])){
+        return false;
+    }
+  }
+
+  }
+
+return true;
+
+}
+
 };
 
 
@@ -54,20 +125,51 @@ function Set() {
 //
 // set.print();
 
-let set = new Set();
-set.add(1);
-console.log(set.values()); //outputs ["1"]
-console.log(set.has(1));
-//outputs true
-console.log(set.size());
-//outputs 1
-set.add(2);
-console.log(set.values()); //outputs ["1", "2"]
-console.log(set.has(2));
-//true
-console.log(set.size());
-//2
-set.remove(1);
-console.log(set.values()); //outputs ["2"]
-set.remove(2);
-console.log(set.values()); //outputs []
+// let set = new Set();
+// set.add(1);
+// console.log(set.values()); //outputs ["1"]
+// console.log(set.has(1));
+// //outputs true
+// console.log(set.size());
+// //outputs 1
+// set.add(2);
+// console.log(set.values()); //outputs ["1", "2"]
+// console.log(set.has(2));
+// //true
+// console.log(set.size());
+// //2
+// set.remove(1);
+// console.log(set.values()); //outputs ["2"]
+// set.remove(2);
+// console.log(set.values()); //outputs []
+
+
+let setA = new Set();
+
+setA.add(1);
+setA.add(2);
+
+
+
+let setB = new Set();
+setB.add(1);
+setB.add(2);
+setB.add(3);
+
+let setC = new Set();
+setC.add(2);
+setC.add(3);
+setC.add(4);
+
+
+//
+// let unionAB = setA.union(setB);
+// let interSectionAB = setA.intersection(setB);
+// let differenceAB = setA.difference(setB);
+
+console.log(setA.subset(setB));
+console.log(setA.subset(setC));
+
+// console.log(unionAB.values());
+// console.log(interSectionAB.values());
+// console.log(differenceAB.values());
